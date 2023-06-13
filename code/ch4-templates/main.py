d78fc5a8310f7d7390e8bdecb636105ba8bb558e
tfn9845 <avv604@gmail.com>
2023-06-13 14:48:17 -0500

    chap4 updates

--- code/ch4-templates/main.py
+++ code/ch4-templates/main.py
@@ -1,11 +1,38 @@
+"""
+DOC String:
+FastAPI Course
+"""
 import fastapi
 import uvicorn
+import fastapi_chameleon
+from fastapi_chameleon import template
 
 app = fastapi.FastAPI()
+fastapi_chameleon.global_init('templates')
+
 
 @app.get('/')
+@template(template_file='index.html')
+def index(user: str = 'Anon-O-moose'):
+    return {
+        'username': user
+    }
+
+@app.get('/')
+def about():
+    return {}
+
+@app.get('/account')
 def index():
-    return "Hello World"
+    return {}
+
+@app.get('/account/login')
+def login():
+    return {}
+
+@app.get('/account/logout')
+def logout():
+    return {}
 
 
 if __name__ == '__main__':
