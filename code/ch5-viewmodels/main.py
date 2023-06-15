052ad08a570f04a542ebf200f47ebd8495dc661b
tfn9845 <avv604@gmail.com>
2023-06-15 12:40:59 -0500

    chap5

---
+++ code/ch5-viewmodels/main.py
@@ -0,0 +1,55 @@
+"""
+DOC String:
+FastAPI Course
+"""
+import fastapi
+import uvicorn
+import fastapi_chameleon
+from starlette.staticfiles import StaticFiles
+# To use static files (CSS, images, etc) you have to do 3 things for Chameleon to work.
+# 1. add above import statement
+# 2. install aiofiles package
+# 3.
+
+from views import home, account, packages
+
+app = fastapi.FastAPI()
+
+
+def main():
+    """
+    Main function to run app. First run configure, then run uvicorn to start up web services.
+    """
+    configure()
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+
+def configure():
+    """
+    Function to run configuration for  templates and routes
+    """
+    configure_templates()
+    configure_route()
+
+
+def configure_templates():
+    """
+    Function to initialize templates directory.
+    """
+    fastapi_chameleon.global_init('templates')
+
+
+def configure_route():
+    """
+    Function to configure routes to various views/templates.
+    """
+    app.mount('/static', StaticFiles(directory='static'), name='static')
+    app.include_router(home.router)
+    app.include_router(account.router)
+    app.include_router(packages.router)
+
+
+if __name__ == '__main__':
+    main()
+else:
+    configure()
---
+++ code/ch5-viewmodels/views/home.py
@@ -0,0 +1,25 @@
+from fastapi_chameleon import template
+import fastapi
+
+router = fastapi.APIRouter()
+@router.get('/')
+# Can use HTML, but chameleon reads .pt files if nothing is given.
+# Example:  @template(template_file='home/index.html')
+@template()
+def index():
+    return {
+        'package_count': 274_999,
+        'release_count': 2_945_123,
+        'user_count': 3_987,
+        'packages': [
+            {'id': 'fastapi', 'summary': "Web framework"},
+            {'id': 'uvicorn', 'summary': "ASGI Server"},
+            {'id': 'httpx', 'summary': "Request for async world"},
+        ]
+    }
+
+@router.get('/about')
+@template()
+# If add about.pt into 'templates' directory, chameleon will auto find file.
+def about():
+    return {}
